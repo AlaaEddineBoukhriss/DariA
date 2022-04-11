5505396aa511aeabb2e1ee6ae62a7c7cc6f3d1a6
AlaaEddineBoukhriss <79084849+AlaaEddineBoukhriss@users.noreply.github.com>
2022-04-11 14:12:50 +0100

    v1

---
+++ src/main/java/tn/esprit/spring/entities/Annonce.java
@@ -0,0 +1,27 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+
+
+@Entity
+public class Annonce implements Serializable {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+	
+	//relation
+	@OneToOne(cascade = CascadeType.ALL, mappedBy="Annonce")
+	private Immobilier Immobilier;
+	@OneToOne(cascade = CascadeType.ALL, mappedBy="Annonce")
+	private Visite Visite;
+}
